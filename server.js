264d5bf954997f396aafce2d248d65658c747deb
Matieyendou Djato <matieydjato@gmail.com>
2024-06-04 14:16:57 +0000

    Logging a User Out

--- server.js
+++ server.js
@@ -47,6 +47,17 @@ myDB(async client => {
     });
   })
 
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
